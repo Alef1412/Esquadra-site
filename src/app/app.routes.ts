8c8e7ac2a1889e84cc6e1af3fd92856ca569ff87
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2024-08-07 22:59:21 -0300

    import fonts and adjusts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {ProductsComponent} from "./products/products.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent },
   { path: '**', redirectTo: 'home' }
 ];
