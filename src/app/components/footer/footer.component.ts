b64f97ce259180a561eac04c5e0c2c94c2177519
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2024-08-10 19:23:15 -0300

    whats app icon implement

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      const quickMessage = this.el.nativeElement.querySelector('#quick-message');
+      if (quickMessage) {
+        this.renderer.setStyle(quickMessage, 'display', 'none');
+      }
+    }, 9000);
+  }
 }
