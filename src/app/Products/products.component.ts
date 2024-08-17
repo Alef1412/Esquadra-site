5e1332e0b38eac94a87a38ad6bc8e2d83013e1f5
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2024-08-17 15:02:16 -0300

    correction in the gallery to pass all photos

--- src/app/Products/products.component.ts
+++ src/app/Products/products.component.ts
@@ -29,16 +29,23 @@ export class ProductsComponent {
   ];
 
   filteredSections = this.sections; // Inicialmente mostra todas as categorias
-
+  allPhotos = this.sections.flatMap(section => section.photos); // Combina todas as fotos de todas as categorias
   isCarouselVisible = false;
   currentPhoto: { src: string, title: string } | null = null;
   currentPhotoIndex = 0;
-  currentSection: { title: string, photos: { src: string, title: string }[] } | null = null;
+  allPhotosMode = false; // Flag para indicar se estamos exibindo todas as fotos
 
   openCarousel(section: { title: string, photos: { src: string, title: string }[] }, index: number) {
-    this.currentSection = section;
+    this.allPhotosMode = false;
     this.currentPhotoIndex = index;
     this.currentPhoto = section.photos[index];
+    this.isCarouselVisible = true;
+  }
+
+  openAllPhotosCarousel(index: number) {
+    this.allPhotosMode = true;
+    this.currentPhotoIndex = index;
+    this.currentPhoto = this.allPhotos[this.currentPhotoIndex];
     this.isCarouselVisible = true;
   }
 
@@ -47,24 +54,32 @@ export class ProductsComponent {
   }
 
   nextPhoto() {
-    if (this.currentSection) {
-      this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.currentSection.photos.length;
-      this.currentPhoto = this.currentSection.photos[this.currentPhotoIndex];
+    if (this.allPhotosMode) {
+      this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.allPhotos.length;
+      this.currentPhoto = this.allPhotos[this.currentPhotoIndex];
+    } else if (this.filteredSections.length === 1) {
+      this.currentPhotoIndex = (this.currentPhotoIndex + 1) % this.filteredSections[0].photos.length;
+      this.currentPhoto = this.filteredSections[0].photos[this.currentPhotoIndex];
     }
   }
 
   prevPhoto() {
-    if (this.currentSection) {
-      this.currentPhotoIndex = (this.currentPhotoIndex - 1 + this.currentSection.photos.length) % this.currentSection.photos.length;
-      this.currentPhoto = this.currentSection.photos[this.currentPhotoIndex];
+    if (this.allPhotosMode) {
+      this.currentPhotoIndex = (this.currentPhotoIndex - 1 + this.allPhotos.length) % this.allPhotos.length;
+      this.currentPhoto = this.allPhotos[this.currentPhotoIndex];
+    } else if (this.filteredSections.length === 1) {
+      this.currentPhotoIndex = (this.currentPhotoIndex - 1 + this.filteredSections[0].photos.length) % this.filteredSections[0].photos.length;
+      this.currentPhoto = this.filteredSections[0].photos[this.currentPhotoIndex];
     }
   }
 
   filterCategory(category: string) {
     if (category === 'all') {
       this.filteredSections = this.sections;
+      this.allPhotosMode = true;
     } else {
       this.filteredSections = this.sections.filter(section => section.title === category);
+      this.allPhotosMode = false;
     }
   }
 }
