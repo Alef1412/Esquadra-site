28a22f47d1d347245a80988ea084bd9e47f11ee5
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2024-08-18 23:17:56 -0300

    alter redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import {AboutComponent} from "./about/about.component";
 import {ProductsComponent} from "./products/products.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductsComponent },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
